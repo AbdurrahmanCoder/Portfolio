1bdeec5acae1cafe3c89edd86d5779dfeeb11af1
AbdurrahmanCoder <abdurrahmanabdulravuf@gmail.com>
2023-12-14 09:28:04 +0100

    added new styles

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,3 +1,4 @@
+ 
 import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
@@ -5,30 +6,32 @@ import emailjs from '@emailjs/browser';
 import '../styling/Contact.css';
 
 const Contact = () => {
- 
-const [show,Setshow] =useState(false);
- 
-useEffect(() => {
-  if (!show) {
-    success();
-  }
-}, [show]); // Add show as a dependency to run the effect when show changes
+  const [show, setShow] = useState(true);
 
-function success() {
-  Setshow(true);
-}
+  useEffect(() => {
+    if (show) { 
+        const timeoutId = setTimeout(() => {
+        setShow(false);
+      }, 3000);
 
-const form = useRef();
+    
+      return () => clearTimeout(timeoutId);
+    }
+  }, [show]);
+
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    // Ensure that the service ID, template ID, and user ID are correct
+ 
     emailjs
       .sendForm('service_d7324c8', 'template_bos3kqi', form.current, 'BW9FnL9rHMNPFrD_i')
       .then(
         (result) => {
           console.log(result.text);
+  
+          setShow(true);
         },
         (error) => {
           console.log(error.text);
@@ -36,28 +39,39 @@ const form = useRef();
       );
   };
 
+  // useEffect(() => {
+
+  //   function work() {
+  //     setShow(true);  
+  //   }
+  
+  //   work();  
+  
+  // }, []); 
+
   return (
     <>
-   
-
- 
-    <div className="Contact" id="Contact">
-     <h1 className='ContactHeading'> CONTACT</h1> 
-      <form className='custom-form'>
-        {/* <form ref={form} onSubmit={sendEmail}> */} 
-        <label >Name</label>
-        <input type="text" className='Control' id='name' name="user_name" required />
-        <label>Email</label>
-        <input type="email"  className='Control' name="user_email" id="email"  required />
-        <label>Message</label>
-        <textarea name="message" className='Control' id="message"  />
-        <input type="submit" value="ENVOYER" /> 
+      <div className="Contact" id="Contact">
+        <h1 className='ContactHeading'> CONTACT</h1>
+        <form className='custom-form' ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" className='Control' id='name' name="user_name" required />
+          <label>Email</label>
+          <input type="email" className='Control' name="user_email" id="email" required />
+          <label>Message</label>
+          <textarea name="message" className='Control' id="message" />
+          <input type="submit" value="ENVOYER" id='Submit' />
         </form>
-
-
-     </div>
+ {show && (
+          <div className="message-container">
+            <p className="message">Message Sent!</p>
+          </div>
+        )}
+       
+      </div>
+     
     </>
   );
 };
-   
-export default Contact;                                                               
+
+export default Contact;

661d0f364e1e6a8d75f09f58892d9d94950278ee
abdurrahmanabdulravuf <abdurrahmanabdulravuf@gmail.com>
2024-06-24 01:11:10 +0200

    update24022024-4

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,6 +1,4 @@
- 
-import React, { useEffect, useState } from 'react';
-import { useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 import '../styling/Contact.css';
@@ -10,11 +8,10 @@ const Contact = () => {
 
   useEffect(() => {
     if (show) { 
-        const timeoutId = setTimeout(() => {
+      const timeoutId = setTimeout(() => {
         setShow(false);
       }, 3000);
 
-    
       return () => clearTimeout(timeoutId);
     }
   }, [show]);
@@ -24,23 +21,20 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
- 
     emailjs
-    .sendForm('service_d7324c8', 'template_bos3kqi', form.current, 'Oq2q7Xbg3Sxq6GRVz')  
-    .then(
+      .sendForm('service_d7324c8', 'template_bos3kqi', form.current, 'Oq2q7Xbg3Sxq6GRVz')  
+      .then(
         (result) => {
           console.log(result.text); 
           setShow(true);
+          form.current.reset();  
         },
         (error) => {
           console.log(error.text);
         }
       );
   };
- 
 
-
- 
   return (
     <>
       <div className="Contact" id="Contact">
@@ -54,14 +48,12 @@ const Contact = () => {
           <textarea name="message" className='Control' id="message" />
           <input type="submit" value="ENVOYER" id='Submit' />
         </form>
- {show && (
+        {show && (
           <div className="message-container">
             <p className="message">Message Sent!</p>
           </div>
         )}
-       
       </div>
-     
     </>
   );
 };
